fec938f0d1e24490e2a2a5f3b424763825615764
catalinbroinas <catalinbroinas@yahoo.com>
2026-02-12 14:52:30 +0200

    feat(GameSettings): add controlled form and handler

--- src/react/components/features/Game/GameSettings.jsx
+++ src/react/components/features/Game/GameSettings.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 
-function GameSettings() {
+function GameSettings({ onApply }) {
+  const [localRange, setLocalRange] = useState({
+    min: null,
+    max: null
+  });
+
   return (
-    <form className="form-container">
+    <form 
+      className="form-container"
+      onSubmit={(e) => {
+        e.preventDefault();
+        onApply(localRange);
+      }}
+    >
       <fieldset className="form-fieldset">
         <legend className="form-legend mb-4">
           <span className="fs-lg">Set range</span>
@@ -14,6 +26,11 @@ function GameSettings() {
               type="number"
               className="form-control"
               placeholder="1"
+              value={localRange.min || ""}
+              onChange={(e) => setLocalRange(prev => ({
+                ...prev,
+                min: Number(e.target.value)
+              }))}
             />
             <label htmlFor="min-range" className="form-label">
               Min
@@ -26,13 +43,25 @@ function GameSettings() {
               type="number"
               className="form-control"
               placeholder="10"
+              value={localRange.max || ""}
+              onChange={(e) => setLocalRange(prev => ({
+                ...prev,
+                max: Number(e.target.value)
+              }))}
             />
             <label htmlFor="max-range" className="form-label">
               Max
             </label>
           </div>
 
-          <button className="btn btn-primary">Apply</button>
+          <button
+            className="btn btn-primary"
+            disabled={
+              !localRange.min ||
+              !localRange.max ||
+              (localRange.min >= localRange.max)
+            }
+          >Apply</button>
         </div>
       </fieldset>
     </form>
