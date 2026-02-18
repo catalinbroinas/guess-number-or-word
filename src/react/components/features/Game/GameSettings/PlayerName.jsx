c2105e0f74fabb1022b5ffe8657cd05a9ff3be02
catalinbroinas <catalinbroinas@yahoo.com>
2026-02-18 13:10:03 +0200

    refactor(PlayerName): remove fieldset and legend

--- src/react/components/features/Game/GameSettings/PlayerName.jsx
+++ src/react/components/features/Game/GameSettings/PlayerName.jsx
@@ -1,27 +1,19 @@
 
 function PlayerName({ name, onNameChange }) {
   return (
-    <fieldset className="form-fieldset">
-      <legend className="form-legend mb-4">
-        <span className="fs-lg">Set player name (optional)</span>
-      </legend>
-
-      <div className="form-row">
-        <div className="form-outline">
-          <input
-            id="player-name"
-            type="text"
-            className="form-control"
-            placeholder="John"
-            value={name}
-            onChange={(e) => onNameChange(e.target.value)}
-          />
-          <label htmlFor="player-name" className="form-label">
-            Name
-          </label>
-        </div>
-      </div>
-    </fieldset>
+    <div className="form-outline">
+      <input
+        id="player-name"
+        type="text"
+        className="form-control"
+        placeholder="John"
+        value={name}
+        onChange={(e) => onNameChange(e.target.value)}
+      />
+      <label htmlFor="player-name" className="form-label">
+        Name
+      </label>
+    </div>
   );
 }
 
